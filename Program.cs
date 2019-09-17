f791769221de527edceb48bdbcefbb0795e8174b
cconstantineau-cdpq <cconstantineau@cdpq.com>
2019-09-17 08:34:47 -0700

    Added sortTopStories transformBlock in order for the next block to have equal partitionning

--- Program.cs
+++ Program.cs
@@ -29,41 +29,66 @@ namespace HackerNews
                  return new ConcurrentQueue<string>(topIds);
              });
 
-            var filterStories = new ActionBlock<ConcurrentQueue<string>>(async queueId =>
+            var filterStories = new TransformBlock<ConcurrentQueue<string>, Item[]>(async queueId =>
             {
-                var maxThreads = 6;
-                int COUNTER = 0;
+                var processCount = Environment.ProcessorCount; //Number of threads equals number of core
+                int topStoryCounter = 0;
+                ConcurrentBag<Item> topStories = new ConcurrentBag<Item>();
 
                 var tasks = new List<Task>();
-                for (int n = 0; n < maxThreads; n++)
+                for (int n = 0; n < processCount; n++)
                 {
                     tasks.Add(Task.Run(async () =>
                     {
-                        while (queueId.TryDequeue(out string id) && COUNTER < 30)
+                        while (queueId.TryDequeue(out string id) && topStoryCounter < Constants.NBSTORIES)
                         {
                             string url = "https://hacker-news.firebaseio.com/v0/item/" + id.ToString() + ".json";
                             string payload = await new HttpClient().GetStringAsync(url);
                             Item item = JsonConvert.DeserializeObject<Item>(payload);
-                            if (item.Type == "story")
+                            if (item.Type == "story" && topStoryCounter < Constants.NBSTORIES)
                             {
-                                Interlocked.Increment(ref COUNTER);
+                                Interlocked.Increment(ref topStoryCounter);
+                                topStories.Add(item);
                             }
-                            System.Console.WriteLine(COUNTER);
                         }
                     }));
                 }
                 await Task.WhenAll(tasks);
+                return topStories.ToArray();
+            });
 
+            var sortTopStories = new TransformBlock<Item[], Item[]>(topStories =>
+            {
+                Array.Sort(topStories, (Item a, Item b) =>
+                {
+                    if (a.Kids == null)
+                        a.Kids = new List<int>();
+
+                    if (b.Kids == null)
+                        b.Kids = new List<int>();
+
+                    if (a.Kids.Count > b.Kids.Count)
+                        return 1;
+
+                    if (a.Kids.Count < b.Kids.Count)
+                        return -1;
+
+                    else
+                        return 0;
+                });
+
+                return topStories;
             });
 
             try
             {
                 var linkOptions = new DataflowLinkOptions { PropagateCompletion = true };
                 getTopStories.LinkTo(filterStories, linkOptions);
+                filterStories.LinkTo(sortTopStories, linkOptions);
 
                 getTopStories.Post("https://hacker-news.firebaseio.com/v0/topstories.json");
                 getTopStories.Complete();
-                filterStories.Completion.Wait();
+                sortTopStories.Completion.Wait();
 
             }
             catch (AggregateException ex)
