c2de3b5b6a9f43c6c8d1ed5883d9df16ae9b6663
ChloeConstantineau <chloe_constantineau@hotmail.com>
2019-09-18 15:13:49 -0700

    Pipeline complete in OO way

--- Program.cs
+++ Program.cs
@@ -1,10 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Threading.Tasks.Dataflow;
-using System.Threading.Tasks;
-using Models;
-using System.Collections.Concurrent;
-using System.Linq;
 using Block;
 
 public static class Constants
@@ -19,58 +14,19 @@ namespace HackerNews
     {
         static void Main(string[] args)
         {
-
-            void printFinalResults(List<TopStory> results)
-            {
-                foreach (var result in results)
-                {
-                    System.Console.Write(result.Title);
-
-                    foreach (var topComment in result.TopComments)
-                    {
-                        //System.Console.Write(" | " + topComment.Key + " (" + topComment.Value + " for story - " + commentsRegistry[topComment.Key] + " total)");
-
-                    }
-                    System.Console.WriteLine();
-                }
-
-            }
-
             try
             {
-
                 var linkOptions = new DataflowLinkOptions { PropagateCompletion = true };
                 string uri = "https://hacker-news.firebaseio.com/v0/topstories.json";
 
-                GetTopStoriesBlock getTopStories = new GetTopStoriesBlock();
-                FilterTopStoriesBlock filterTopStories = new FilterTopStoriesBlock();
-                TraverseTopStoriesCommentsBlock traverseTopStoriesComments = new TraverseTopStoriesCommentsBlock();
-                ComputeTopCommentsPerStoryBlock computeTopCommentsPerStory = new ComputeTopCommentsPerStoryBlock();
-
-                getTopStories.block.LinkTo(filterTopStories.block, linkOptions);
-                filterTopStories.bufferBlock.LinkTo(traverseTopStoriesComments.block, linkOptions);
-                traverseTopStoriesComments.bufferBlock.LinkTo(computeTopCommentsPerStory.block, linkOptions);
-                getTopStories.block.Post(uri);
-                getTopStories.block.Complete();
-
-                List<TopStory> finalResults = new List<TopStory>();
-                var receiveAllStories = Task.Run(() =>
-                   {
-                       for (int i = 0; i <= Constants.NBSTORIES; i++)
-                       {
-                           finalResults.Add(computeTopCommentsPerStory.bufferBlock.Receive());
-                       }
-                   });
-                Task.WaitAll(receiveAllStories);
-                computeTopCommentsPerStory.bufferBlock.Complete();
-                printFinalResults(finalResults);
+                Pipeline hackerNewsPipeline = new Pipeline(uri, linkOptions);
+                hackerNewsPipeline.Run(uri);
 
             }
             catch (AggregateException ex)
             {
                 System.Console.WriteLine(ex.Flatten());
             }
-
         }
     }
 }
---
+++ blocks/Pipeline.cs
@@ -0,0 +1,66 @@
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using System.Threading.Tasks.Dataflow;
+using Models;
+
+namespace Block
+{
+    class Pipeline
+    {
+        public Pipeline(string uri, DataflowLinkOptions linkOptions)
+        {
+            this.uri = uri;
+            this.linkOptions = linkOptions;
+            this.topStories = new List<TopStory>();
+            this.commentsRegistry = new ConcurrentDictionary<string, int>();
+        }
+
+        public void Run(string uri)
+        {
+            GetTopStoriesBlock getTopStories = new GetTopStoriesBlock();
+            FilterTopStoriesBlock filterTopStories = new FilterTopStoriesBlock();
+            TraverseTopStoriesCommentsBlock traverseTopStoriesComments = new TraverseTopStoriesCommentsBlock();
+            ComputeTopCommentsPerStoryBlock computeTopCommentsPerStory = new ComputeTopCommentsPerStoryBlock();
+
+            getTopStories.block.LinkTo(filterTopStories.block, this.linkOptions);
+            filterTopStories.bufferBlock.LinkTo(traverseTopStoriesComments.block, linkOptions);
+            traverseTopStoriesComments.bufferBlock.LinkTo(computeTopCommentsPerStory.block, linkOptions);
+            getTopStories.block.Post(uri);
+            getTopStories.block.Complete();
+
+            var receiveAllStories = Task.Run(() =>
+               {
+                   for (int i = 0; i <= Constants.NBSTORIES; i++)
+                   {
+                       this.topStories.Add(computeTopCommentsPerStory.bufferBlock.Receive());
+                   }
+               });
+
+            Task.WaitAll(receiveAllStories);
+            computeTopCommentsPerStory.bufferBlock.Complete();
+            this.commentsRegistry = traverseTopStoriesComments.commentsRegistry;
+            printResults(this.topStories);
+        }
+
+        void printResults(List<TopStory> results)
+        {
+            foreach (var result in results)
+            {
+                System.Console.Write(result.Title);
+
+                foreach (var topComment in result.TopComments)
+                {
+                    System.Console.Write(" | " + topComment.Key + " (" + topComment.Value + " for story - " + commentsRegistry[topComment.Key] + " total)");
+                }
+                System.Console.WriteLine();
+            }
+
+        }
+
+        string uri { get; set; }
+        DataflowLinkOptions linkOptions { get; set; }
+        List<TopStory> topStories { get; set; }
+        ConcurrentDictionary<string, int> commentsRegistry { get; set; }
+    }
+}
