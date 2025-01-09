1fccfbfe3952891886b79e6d4c91639ad43afe55
Chloe Constantineau <chloe_constantineau@hotmail.com>
2025-01-09 15:21:34 -0500

    Fix tests

--- Tests/PipelineTests.cs
+++ Tests/PipelineTests.cs
@@ -3,14 +3,7 @@ using System.Threading.Tasks.Dataflow;
 using System.Collections.Generic;
 using System.Collections.Concurrent;
 using HackerNews.blocks;
-using HackerNews;
-using System;
-using RichardSzalay.MockHttp;
-using Newtonsoft.Json;
 using HackerNews.models;
-
-//* This project tests all four blocks used in the pipeline solution *//
-//* Future iteration should mock the api service                     *//
 
 namespace Tests
 {
@@ -18,27 +11,26 @@ namespace Tests
     [TestClass]
     public class PipelineTests
     {
-        [TestMethod]
-        public void GetTopStoriesBlock()
-        {
-            // Setup Mock
-            var mockHttp = new MockHttpMessageHandler();
-            List<int> expectedResponse = [1, 2, 3, 4, 5];
-            string uri = "https://hacker-news.firebaseio.com/v0/topstories.json";
-            mockHttp.When(uri)
-                    .Respond("application/json", JsonConvert.SerializeObject(expectedResponse));
+        public DataflowLinkOptions LinkOptions = new() { PropagateCompletion = true };
 
+        [TestMethod]
+        public void Fetch_Top_Stories()
+        {
+            // Data
+            var path = "/v0/topstories.json";
+            List<int> expectedResponse = [1, 2, 3, 4, 5];
+
+            // Setup Mock Client
+            var client = TestUtils.SetupClient([(path, expectedResponse)]);
 
             // Setup Test
-            var client = new HackerNewsClient(mockHttp.ToHttpClient());
-            var linkOptions = new DataflowLinkOptions { PropagateCompletion = true };
-
             GetTopStories getTopStoriesBlock = new(client);
             BufferBlock<ConcurrentBag<int>> resultBlock = new();
 
-            getTopStoriesBlock.Block.LinkTo(resultBlock, linkOptions);
-            getTopStoriesBlock.Block.Post(uri);
+            getTopStoriesBlock.Block.LinkTo(resultBlock, LinkOptions);
+            getTopStoriesBlock.Block.Post(path);
             getTopStoriesBlock.Block.Complete();
+            getTopStoriesBlock.Block.Completion.Wait();
 
             // Assert
             var expectedResult = new ConcurrentBag<int>(expectedResponse);
@@ -48,34 +40,20 @@ namespace Tests
 
 
         [TestMethod]
-        public void FilterTopStoriesBlock()
+        public void Filter_Top_Stories()
         {
-
-            var mockHttp = new MockHttpMessageHandler();
-            var baseUri = "https://hacker-news.firebaseio.com";
-            void setupMockItem(Item item)
-            {
-                var uri = baseUri + "/v0/item/" + item.Id.ToString() + ".json";
-                mockHttp.When(uri).Respond("application/json", JsonConvert.SerializeObject(item));
-            }
-
-            // Setup Mock
+            // Data
             Item item1 = new(Id: 1, Type: "story");
             Item item2 = new(Id: 2, Type: "comment");
-            setupMockItem(item1);
-            setupMockItem(item2);
 
-            // Setup HttpClient
-            var httpClient = mockHttp.ToHttpClient();
-            httpClient.BaseAddress = new Uri(baseUri);
-            var client = new HackerNewsClient(httpClient);
-            
+            // Setup Mock Client
+            var client = TestUtils.SetupClient([(TestUtils.ItemPath(item1), item1), (TestUtils.ItemPath(item2), item2)]);
+
             // Setup Test
             FilterTopStories filterTopStoriesBlock = new(client);
             BufferBlock<ConcurrentBag<Item>> resultBlock = new();
-            var linkOptions = new DataflowLinkOptions { PropagateCompletion = true };
 
-            filterTopStoriesBlock.Block.LinkTo(resultBlock, linkOptions);
+            filterTopStoriesBlock.Block.LinkTo(resultBlock, LinkOptions);
             filterTopStoriesBlock.Block.Post([item1.Id, item2.Id]);
             filterTopStoriesBlock.Block.Complete();
             filterTopStoriesBlock.Block.Completion.Wait();
@@ -88,48 +66,30 @@ namespace Tests
             Assert.AreEqual(item1, actualItem);
         }
 
-        // [TestMethod]
-        // public void TraverseTopStoriesCommentsBlock()
-        // {
-        //     Item story = new(21021709)
-        //     {
-        //         Kids = new List<int>(21021916),
-        //         Dead = false,
-        //         Deleted = false
-        //     };
+        [TestMethod]
+        public void Compute_Comments_Per_Top_Story()
+        {
+            // Data
+            Item item1 = new(Id: 1, Type: "story", Kids: [2]);
+            Item item2 = new(Id: 2, Type: "comment", By: "Some Author", Kids: [3, 4]);
+            Item item3 = new(Id: 3, Type: "comment", By: "Some Other Author");
+            Item item4 = new(Id: 4, Type: "comment", By: "Another Author");
 
-        //     GetComments traverseTopStoriesComments = new();
-        //     traverseTopStoriesComments.Block.Post(story);
+            // Setup Mock Client
+            var client = TestUtils.SetupClient([(TestUtils.ItemPath(item1), item1), (TestUtils.ItemPath(item2), item2), (TestUtils.ItemPath(item3), item3), (TestUtils.ItemPath(item4), item4)]);
 
-        //     var result = traverseTopStoriesComments.BufferBlock.ReceiveAsync().Result;
-        //     Assert.IsNotNull(result.Comments);
-        // }
+            // Setup Test
+            GetComments computeComments = new(client);
+            BufferBlock<ConcurrentBag<TopStory>> resultBlock = new();
+            computeComments.Block.LinkTo(resultBlock, LinkOptions);
+            computeComments.Block.Post([item1]);
 
-        // [TestMethod]
-        // public void ComputeTopCommentsPerStoryBlock()
-        // {
-        //     ConcurrentDictionary<string, int> comments = new();
-        //     comments.TryAdd("a", 1);
-        //     comments.TryAdd("b", 2);
-        //     comments.TryAdd("c", 3);
-        //     comments.TryAdd("d", 4);
-        //     comments.TryAdd("e", 5);
-        //     comments.TryAdd("f", 6);
-        //     comments.TryAdd("g", 7);
-        //     comments.TryAdd("h", 8);
-        //     comments.TryAdd("i", 9);
-        //     comments.TryAdd("j", 10);
-        //     comments.TryAdd("k", 11);
-        //     comments.TryAdd("l", 12);
+            var actualResult = resultBlock.ReceiveAsync().Result;
 
-        //     TopStory topStory = new("US takes 'richest nation on earth' crown from Switzerland", comments, []);
-        //     maybe traverseTopStoriesComments = new();
-        //     traverseTopStoriesComments.Block.Post(topStory);
-
-        //     var topStoryWithTopComments = traverseTopStoriesComments.BufferBlock.ReceiveAsync().Result;
-
-        //     Assert.IsNotNull(topStoryWithTopComments.TopComments);
-        //     Assert.AreEqual(Constants.NB_TOP_COMMENTERS, topStoryWithTopComments.TopComments.Count);
-        // }
+            // Asserts
+            Assert.AreEqual(1, actualResult.Count);
+            actualResult.TryPeek(out TopStory actualStory);
+            Assert.AreEqual(3, actualStory.Comments.Count);
+        }
     }
 }
---
+++ Tests/TestUtils.cs
@@ -0,0 +1,29 @@
+using System;
+using System.Collections.Generic;
+using HackerNews;
+using HackerNews.models;
+using Newtonsoft.Json;
+using RichardSzalay.MockHttp;
+
+namespace Tests
+{
+    public class TestUtils()
+    {
+        static public HackerNewsClient SetupClient<T>(List<(string, T)> expectedResponses)
+        {
+            var mockHttp = new MockHttpMessageHandler();
+            var baseUri = "https://hacker-news.firebaseio.com";
+            expectedResponses.ForEach(r =>
+            {
+                var uri = baseUri + r.Item1;
+                mockHttp.When(uri).Respond("application/json", JsonConvert.SerializeObject(r.Item2));
+            });
+
+            var httpClient = mockHttp.ToHttpClient();
+            httpClient.BaseAddress = new Uri(baseUri);
+            return new HackerNewsClient(httpClient);
+        }
+
+        static public string ItemPath(Item item) => "/v0/item/" + item.Id.ToString() + ".json";
+    }
+}
